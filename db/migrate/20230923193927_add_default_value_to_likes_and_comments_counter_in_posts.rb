1ab20876a88afd053646f9848cc1908a81bd3f9e
Riz <mahmoud.eleresousy@gmail.com>
2023-09-23 22:43:07 +0300

    Add and run migrations to update default values for counters in Users and Posts tables

---
+++ db/migrate/20230923193927_add_default_value_to_likes_and_comments_counter_in_posts.rb
@@ -0,0 +1,6 @@
+class AddDefaultValueToLikesAndCommentsCounterInPosts < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :posts, :likesCounter, 0
+    change_column_default :posts, :commentsCounter, 0
+  end
+end
