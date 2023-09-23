abf5e0c4943fcb3408408cbfa1bef8f89ad7cb06
Riz <mahmoud.eleresousy@gmail.com>
2023-09-23 23:05:30 +0300

    Add models, quiries, and methods

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    after_save :update_post_comments_count
+
+    private
+
+    def update_post_comments_count
+        post_comments_count = Comment.where(post_id: :post_id).count
+        post.update(commentsCounter: post_comments_count)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ApplicationRecord
+    belongs_to 'author', class_name: 'User'
+
+    has_many :comments, foreign_key: 'post_id', dependent: :destroy
+    has_many :likes, foreign_key: 'post_id' dependent: :destroy
+    
+    after_save :update_user_posts_counter
+
+    def recent_comments
+        Comment.where(post_id: id).order(created_at: :desc).limit(5)
+    end
+
+    private
+
+    def update_user_posts_counter
+        user_posts_count = Post.where(author_id: :author_id).count
+        author.update(postsCounter: user_posts_count)
+    end
+end
