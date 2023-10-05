d5c9365703eada2a2dd1733256f54b121fed01d8
Riz <mahmoud.eleresousy@gmail.com>
2023-10-05 20:09:18 +0300

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, recoverable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+         :rememberable, :validatable, :confirmable, :recoverable
+         
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'user_id', dependent: :destroy
   has_many :likes, foreign_key: 'user_id', dependent: :destroy
